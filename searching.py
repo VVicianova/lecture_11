a716b1f1680b7dca29eb693f95a310b113c7874b
VVicianova <78555062+VVicianova@users.noreply.github.com>
2021-04-19 10:17:59 +0200

    hello

--- searching.py
+++ searching.py
@@ -1,4 +1,6 @@
 import os
+import json
+
 
 # get current working directory path
 cwd_path = os.getcwd()
@@ -12,10 +14,22 @@ def read_data(file_name, field):
     :return: (list, string),
     """
     file_path = os.path.join(cwd_path, file_name)
+    # with open(file_path) as f:
+    #     data = dict(json.load(f))
+    #     if field in data.keys():
+    #         return field
+    #     # ???
+    #     else:
+    #         return None
+    with open(file_path, "r") as json_file:
+        seq = json.load(json_file)
+        return seq[field]
+#
 
 
 def main():
-    pass
+    sequential_data = print(read_data("sequential.json", "unordered_numbers"))
+    return sequential_data
 
 
 if __name__ == '__main__':
